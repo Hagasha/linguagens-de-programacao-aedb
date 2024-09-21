3712c4f6e8c3de84a10d99add1dc396808e9a6be
Rayan Diniz <84201696+Hagasha@users.noreply.github.com>
2024-09-21 13:17:22 -0300

    Atualizando o código para implementar o banco de dados

---
+++ projeto-aedb1/Interfaces/IRepositorioEditora.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using projeto_aedb1.Model;
+
+
+namespace projeto_aedb1.Interfaces
+{
+    public interface IRepositorioEditora
+    {
+        Editora Incluir(Editora editora);
+
+        void Alterar(Editora editora);
+
+        void Excluir(Editora editora);
+
+        void Excluir(int id);
+
+        List<Editora> ListarTodos();
+        Editora Selecionar(int id);
+    }
+}
